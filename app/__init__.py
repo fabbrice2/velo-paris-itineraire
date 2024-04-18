cc92b6f299f2308b56b3ee1f2b37afcdf1d2814a
fabbrice2 <fabriceamenouglo@gmail.com>
2024-04-18 19:11:26 +0200

    Modification and addition of the profile modification part

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-# Importez vos routes depuis routes.py
+app.secret_key = os.urandom(24)
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Génération d'une clé secrète aléatoire
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,97 @@
-from flask import render_template   
-from app import app               
+from app import app
+from flask import Flask, redirect, render_template, session, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import db
 
-@app.route('/')
-def index():
-    return render_template('index.html.jinja')
+cursor = db.cursor()
+
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    if "username" in session:
+        username = session["username"]
+        return render_template("home.html.jinja", username=username)
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    errormsg = ""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        cursor.execute("SELECT * FROM accounts where username = %s", (username,))
+        user = cursor.fetchone()
+
+        if user and check_password_hash(user[2], password):
+            session["id"] = user[0]
+            session["username"] = user[1]
+            session["email"] = user[3]
+            session["password"] = user[2]
+            return redirect(url_for("home"))
+        else:
+            errormsg = "Email ou mot de passe incorrect"
+
+    return render_template("login.html.jinja", errormsg=errormsg)
+
+
+@app.route("/register/", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = generate_password_hash(request.form["password"])
+
+        cursor.execute(
+            "INSERT INTO accounts (username, email, password) VALUES (%s, %s, %s)",
+            (username, email, password),
+        )
+        db.commit()
+
+        return redirect(url_for("login"))
+
+    return render_template("register.html.jinja")
+
+
+@app.route("/logout/")
+def logout():
+    session.pop("username")
+    session.pop("email")
+    session.pop("password")
+    session.pop("id")
+    return redirect(url_for("login"))
+
+
+@app.route("/profil/")
+def profil():
+    if "username" in session:
+        username = session["username"]
+        email = session["email"]
+        password = session["password"]
+    return render_template(
+        "profil.html.jinja", username=username, email=email, password=password
+    )
+
+
+@app.route("/updateProfil/", methods=["POST", "GET"])
+def updateProfil():
+    if "id" not in session:
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+
+        cursor.execute(
+            "UPDATE accounts SET username = %s, email = %s WHERE id = %s",
+            (username, email, session["id"]),
+        )
+        db.commit()
+        session["username"] = username
+        session["email"] = email
+
+        return redirect(url_for("home"))
+
+    return render_template("updateProfil.html.jinja")
