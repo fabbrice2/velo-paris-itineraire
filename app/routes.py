aa10e0285c922a1d48a7c83b34fec5d30361c891
Aboubakar Bamba <aboubakarbamba716@gmail.com>
2024-04-25 13:19:47 +0200

    Ajout de la section favoris

--- app/routes.py
+++ app/routes.py
@@ -44,32 +44,25 @@ def home():
     return render_template("home.html.jinja")
 
 def get_data_from_url():
-    # Modification de Aboubakar
-    global cached_data, Last_Update_Time
 
-    # Modification de Aboubakar
+    global cached_data, Last_Update_Time
     cached_data = None
     Last_Update_Time = None
 
-
-    # Modification de Aboubakar
     if cached_data is not None and Last_Update_Time is not None:
         if time.time() - Last_Update_Time > 300:
-            print("Les données en cache sont périmées. Récupération de nouvelles données...")
+            print("les donnee en cache sont perime recuperations de nouvelle donne")
             return cached_data 
-
     url = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/velib-disponibilite-en-temps-reel/records?limit=20"
     response = requests.get(url)
-
     if response.status_code == 200:
         data = response.json()
-        # Modification de Aboubakar
         cached_data = data['results']
         Last_Update_Time = time.time()
-        print("Nouvelles données récupérées et mises en cache.")
+        print("nouvelle donnee recuperer et mise en cache.")
         return data['results']
     else:
-        print("La récupération des données a échoué.")
+        print("la recuperations des donnee a echouer.")
         return None
 
 
@@ -111,9 +104,6 @@ def login():
             session["username"] = user[1]
             session["email"] = user[3]
             session["password"] = user[2]
-            
-            # Afficher l'ID de l'utilisateur
-            print(f"ID de l'utilisateur : {user[0]}")
             
             return redirect(url_for("home"))
         else:
@@ -180,17 +170,11 @@ def updateProfil():
 
     return render_template("updateProfil.html.jinja")
 
-from flask import session
-
-from flask import session
-
-from flask import request
-
 @app.route('/favorites', methods=['POST'])
 def add_favorite():
     if 'id' in session:
+
         user_id = session['id']
-        
         station_name = request.form.get('station_name')
         station_id = request.form.get('station_id')
 
@@ -202,13 +186,39 @@ def add_favorite():
                 (station_name, user_id, station_id)
             )
             db.commit()
-            print("Données de favoris insérées avec succès.")
+            print("donnee insere avec succes .")
             return redirect(request.referrer)
         except Exception as e:
-            print(f"Erreur lors de l'insertion des données de favoris : {e}")
-            return jsonify({"error": "Erreur lors de l'insertion des données de favoris."}), 500
+            print(f"erreur lors de l'insertion : {e}")
+            return jsonify({"error": "erreur lors de l'insertion."}), 500
     else:
         return redirect(url_for('login'))
+    
+
+@app.route('/favorites/')
+def show_favorites():
+    if 'id' in session:
+        user_id = session['id']
+        try:
+            cursor.execute("SELECT * FROM favorite_relations WHERE user_id = %s", (user_id,))
+            favorites = cursor.fetchall()
+
+            return render_template('favorites.html.jinja', favorites=favorites)
+        except Exception as e:
+            return jsonify({"error": "Erreur lors de la récupération des favoris."}), 500
+    else:
+        return redirect(url_for('login'))
+
+    
+    
+
+from flask import session
+
+from flask import session
+
+from flask import request
+
+
 
 
 
